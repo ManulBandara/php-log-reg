3289b1e87f40c36617ac83f94a9474c0c0c4435c
Manul Bandara <manulbandara@gmail.com>
2024-01-29 13:15:32 +0530

    first commit

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php include('header.php');?>
+
+<?php
+// Initialize the session
+session_start();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
+
+<?php include('footer.php');?>
